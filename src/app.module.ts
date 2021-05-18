29df850f7797546e0b597a5db45a049e4cab490e
DaigoHonda <daigo.honda@ncdc.co.jp>
2021-05-18 15:36:37 +0900

    Fix: startが通らなかったので修正しました

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContentModule } from './content/content.module';
-import { ContentTreeController } from './content-tree/content-tree.controller';
 import { ContentTreeModule } from './content-tree/content-tree.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), ContentModule, ContentTreeModule],
-  controllers: [AppController, ContentTreeController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/content-tree/content-tree.controller.ts
+++ src/content-tree/content-tree.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { ContentTreeService } from './content-tree.service';
+import { ContentTree } from './../entities/content-tree.entity';
 
 @Controller('content-tree')
-export class ContentTreeController {}
+export class ContentTreeController {
+  constructor(private readonly service: ContentTreeService) {}
+  @Get()
+  async getAllContentTreeList(): Promise<ContentTree[]> {
+    return await this.service.find();
+  }
+}
